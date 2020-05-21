c7f2164e3e551abbeb15a44464f4411e3b7635e2
Đậu Quang Lộc <63194262+LocDauMDR@users.noreply.github.com>
2020-05-21 10:09:07 +0700

    V1.0

---
+++ app/src/main/java/vn/edu/ntu/quangloc/controller/CartController.java
@@ -0,0 +1,27 @@
+package vn.edu.ntu.quangloc.controller;
+
+import android.app.Application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import vn.edu.ntu.quangloc.model.Product;
+
+public class CartController extends Application implements ICartController {
+
+    List<Product> listProducts = new ArrayList<>();
+
+    public CartController() {
+        listProducts.add(new Product("Khoai Lang",25000, "Khoai lang theo tiêu chuẩn VietGap"));
+        listProducts.add(new Product("Khoai Sọ",75000, "Khoai sọ trồng tại Ninh Hòa"));
+        listProducts.add(new Product("Khoai Tím",65000, "Khoai tím màu đỏ đỏ tươi"));
+        listProducts.add(new Product("Sầu Riêng",95000, "Sầu riêng Khánh Sơn loại 1"));
+        listProducts.add(new Product("Bưởi da xanh",45000, "Bưởi da xanh Khánh Sơn loại 1"));
+        listProducts.add(new Product("Bưởi da vàng",35000, "Bưởi da vàng Khánh Sơn loại 1"));
+    }
+
+    @Override
+    public List<Product> getAllProduct() {
+        return listProducts;
+    }
+}
---
+++ app/src/main/java/vn/edu/ntu/quangloc/controller/ICartController.java
@@ -0,0 +1,9 @@
+package vn.edu.ntu.quangloc.controller;
+
+import java.util.List;
+
+import vn.edu.ntu.quangloc.model.Product;
+
+public interface ICartController {
+    public List<Product> getAllProduct();
+}
